d023a18e269ef7c5b3419281bc29216357c4539b
Henry <henrykc24@gmail.com>
2022-05-25 17:04:46 +0100

    Add get single tour route

--- app.js
+++ app.js
@@ -8,16 +8,6 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this URL.');
-// });
-
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -28,22 +18,47 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'failure',
+      data: {
+        message: 'No tour with the provided id!',
+      },
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/d-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
-    if (err) {
-      res.status(500).json(err);
-    } else {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
+  fs.writeFile(
+    `${__dirname}/d-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).json(err);
+      } else {
+        res.status(201).json({
+          status: 'success',
+          data: {
+            tour: newTour,
+          },
+        });
+      }
     }
-  })
+  );
 });
 
 const port = 4000;
