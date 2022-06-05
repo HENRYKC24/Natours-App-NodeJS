a857b90d19fb350b4a87cd6e8128b4a7456399e7
Henry <henrykc24@gmail.com>
2022-06-05 15:50:59 +0100

    Install mongoose and connect database

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.MONGODB_REMOTE_SERVER.replace(
+  '<PASSWORD>',
+  process.env.MONGODB_PASSWORD
+);
+mongoose
+  // .connect(process.env.MONGODB_LOCAL_SERVER, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connection successful'));
 
 const app = require('./app');
 // RUN SERVER
