17a03eb57c021843b8045436aebbb33e1435eb16
Henry <henrykc24@gmail.com>
2022-06-11 11:44:18 +0100

    Make APIFeatures class modular

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/APIFeatures');
 
 // REQUEST HANDLERS
 
@@ -8,64 +9,6 @@ exports.top5Cheapest = async (req, res, next) => {
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
-
-class APIFeatures {
-  constructor(mongoQueryObject, requestQueryObject) {
-    this.mongoQueryObject = mongoQueryObject;
-    this.requestQueryObject = requestQueryObject;
-  }
-
-  filter() {
-    // BUILD QUERY
-    const queryObj = { ...this.requestQueryObject };
-    const specialQueries = ['page', 'sort', 'limit', 'fields'];
-    specialQueries.forEach((each) => delete queryObj[each]);
-
-    // COMPLEX FILTERING
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(
-      /\b(lt|gt|lte|gte)\b/g,
-      (match) => `$${match}`
-    );
-    this.mongoQueryObject = this.mongoQueryObject.find(JSON.parse(queryString));
-    return this;
-  }
-
-  //  SORT QUERY
-  sort() {
-    if (this.requestQueryObject.sort) {
-      this.mongoQueryObject = this.mongoQueryObject.sort(
-        this.requestQueryObject.sort.split(',').join(' ')
-      );
-    } else {
-      this.mongoQueryObject = this.mongoQueryObject.sort('createdAt');
-    }
-    return this;
-  }
-
-  // FIELD LIMITING
-  limitFields() {
-    const { fields } = this.requestQueryObject;
-
-    if (fields) {
-      this.mongoQueryObject = this.mongoQueryObject.select(
-        fields.split(',').join(' ')
-      );
-    } else {
-      this.mongoQueryObject = this.mongoQueryObject.select('-__v');
-    }
-    return this;
-  }
-
-  // PAGINATION
-  paginate() {
-    const page = this.requestQueryObject.page * 1 || 1;
-    const limit = this.requestQueryObject.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    this.mongoQueryObject = this.mongoQueryObject.skip(skip).limit(limit);
-    return this;
-  }
-}
 
 exports.createTour = async (req, res) => {
   try {
