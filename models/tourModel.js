0f0737bab44018996563a095a9832596257ea4e1
Henry <henrykc24@gmail.com>
2022-06-16 20:54:24 +0100

    Use custom validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less than or equal 40 characters'],
       minlength: [10, 'A tour name must have more than or equal 10 characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: String,
     duration: {
@@ -42,7 +44,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // This only point to the current document creation
+          return this.price > val;
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
@@ -78,7 +89,7 @@ tourSchema.virtual('durationWeeks').get(function () {
 });
 
 // Define mongoose middleware
-// DOCUMENT MIDDLEWARE
+// DOCUMENT MIDDLEWARE: runs for .save() and .create() methods
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
