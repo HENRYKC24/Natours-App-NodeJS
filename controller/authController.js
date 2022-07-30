6a040a8bb1288a6b74ec12d552122c50a3dc7b0e
Henry <henrykc24@gmail.com>
2022-07-30 21:46:17 +0100

    Add send cookie feature

--- controller/authController.js
+++ controller/authController.js
@@ -15,6 +15,20 @@ const getJWTToken = (id) =>
 const sendResponseWithToken = (user, statusCode, res) => {
   const token = getJWTToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
